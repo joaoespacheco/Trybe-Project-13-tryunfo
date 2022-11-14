4eb83bfc97290de700ca44751fe86467ba8f616b
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-11-14 18:22:39 -0300

    Alterações no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
 
+const InitialState = {
+  cardName: '',
+  cardDescription: '',
+  cardAttr1: '0',
+  cardAttr2: '0',
+  cardAttr3: '0',
+  cardImage: '',
+  cardRare: 'normal',
+  cardTrunfo: false,
+};
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      cardName: '',
-      cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
-      cardImage: '',
-      cardRare: '',
-      cardTrunfo: false,
+      ...InitialState,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
       deckOfcards: [],
@@ -41,17 +45,10 @@ class App extends React.Component {
 
   saveCardOnDeck() {
     const {
-      cardName,
-      cardDescription,
-      cardImage,
-      cardRare,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardTrunfo,
-      hasTrunfo,
+      cardName, cardDescription, cardImage, cardRare, cardAttr1,
+      cardAttr2, cardAttr3, cardTrunfo, hasTrunfo,
     } = this.state;
-    const newCart = {
+    const newCard = {
       cardName,
       cardDescription,
       cardImage,
@@ -61,34 +58,20 @@ class App extends React.Component {
       cardAttr3,
       cardTrunfo,
     };
-    const stateReset = [
-      { target: { name: 'cardName', value: '' } },
-      { target: { name: 'cardDescription', value: '' } },
-      { target: { name: 'cardImage', value: '' } },
-      { target: { name: 'cardRare', value: 'normal' } },
-      { target: { name: 'cardAttr1', value: '0' } },
-      { target: { name: 'cardAttr2', value: '0' } },
-      { target: { name: 'cardAttr3', value: '0' } },
-      { target: { name: 'cardTrunfo', value: false } },
-    ];
+
     this.setState(
       (previousState) => ({
-        deckOfcards: [...previousState.deckOfcards, newCart],
+        deckOfcards: [...previousState.deckOfcards, newCard],
       }),
-      () => stateReset.forEach((target) => this.handleChanger(target)),
+      () => this.setState(({ ...InitialState })),
     );
     if (hasTrunfo === false) this.setState({ hasTrunfo: cardTrunfo });
   }
 
   checkState() {
     const {
-      cardName,
-      cardDescription,
-      cardImage,
-      cardRare,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
+      cardName, cardDescription, cardImage, cardRare,
+      cardAttr1, cardAttr2, cardAttr3,
     } = this.state;
     const valueMax = 90;
     const valueMin = 0;
@@ -150,16 +133,7 @@ class App extends React.Component {
     const newObjetc = deckOfcards.filter((chave) => funcao(chave));
     const retorno = newObjetc.map((carta) => (
       <div key={ carta.cardName }>
-        <Card
-          cardName={ carta.cardName }
-          cardDescription={ carta.cardDescription }
-          cardAttr1={ carta.cardAttr1 }
-          cardAttr2={ carta.cardAttr2 }
-          cardAttr3={ carta.cardAttr3 }
-          cardImage={ carta.cardImage }
-          cardRare={ carta.cardRare }
-          cardTrunfo={ carta.cardTrunfo }
-        />
+        <Card { ...carta } />
         <button
           data-testid="delete-button"
           id={ carta.cardName }
@@ -176,10 +150,7 @@ class App extends React.Component {
   }
 
   render() {
-    const {
-      filterText,
-      filterDisable,
-    } = this.state;
+    const { filterText, filterDisable } = this.state;
 
     return (
       <div>
