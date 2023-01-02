8175ec5ca147eb98f79af99be8d738c7573d27d6
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2023-01-02 18:50:32 -0300

    Estilização do header e formulário concluídas

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,7 +14,7 @@ class Card extends React.Component {
       cardTrunfo,
     } = this.props;
     return (
-      <div>
+      <div className="card-container">
         <h2 data-testid="name-card">{ cardName }</h2>
         <img data-testid="image-card" src={ cardImage } alt={ cardName } />
         <p data-testid="description-card">{ cardDescription }</p>
